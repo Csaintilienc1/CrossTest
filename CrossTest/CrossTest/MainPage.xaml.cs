77c808f6cff52aa586a957cc40c9f1ff67982762
Saintilienc1 <Saintilienclaudel@gmail.com>
2018-05-21 01:58:16 -0400

    Add project files.

---
+++ CrossTest/CrossTest/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrossTest
+{
+    public partial class MainPage : ContentPage
+    {
+        static string bomb = new Random().Next(1, 4).ToString();
+        int scores = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text==bomb)
+            {
+               scores = 0;
+               await DisplayAlert("Verdict", "Game Over", "Cancel");
+               bomb = new Random().Next(1, 4).ToString();
+            }
+            else
+            {
+                scores++;
+                await DisplayAlert("Keep Going","Scores"+scores,"Continue");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+        }
+
+    }
+}
